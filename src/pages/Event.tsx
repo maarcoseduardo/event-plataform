8880ab820ba2faf7ac3aaf5a58afe9fca5adcf46
Marcos Eduardo <bacalhau_1995@hotmail.com>
2022-06-28 11:58:19 -0300

    edit: local-variables and css-subscribe

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -3,10 +3,6 @@ import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 import { Video } from "../components/Video";
 
-interface Params {
-  slug: string;
-}
-
 export function Event() {
 
   const { slug } = useParams<{slug: string}>();
