2189b9e249cf40ab79e6d5da8d17c356d6216b66
Marcos Eduardo <bacalhau_1995@hotmail.com>
2022-07-05 16:00:35 -0300

    edit: the final config of structure

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -41,7 +41,7 @@ export function Video({ lessonSlug }: VideoProps) {
     }
   })
 
-  if(!data) {
+  if (!data || !data.lesson) {
     return (
       <div className="flex-1">
         <h1>Carregando...</h1>
@@ -68,17 +68,19 @@ export function Video({ lessonSlug }: VideoProps) {
             <p className="mt-4 text-gray-200 leading-relaxed">
               {data.lesson.description}
             </p>
-            <div className="flex items-center gap-4 mt-6">
-              <img
-                className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src={data.lesson.teacher.avatarURL}
-                alt=""
-              />
-              <div className="leading-relaxed">
-                <strong className="font-bold font-2xl block">{data.lesson.teacher.name}</strong>
-                <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
+            {data.lesson.teacher && (
+              <div className="flex items-center gap-4 mt-6">
+                <img
+                  className="h-16 w-16 rounded-full border-2 border-blue-500"
+                  src={data.lesson.teacher.avatarURL}
+                  alt=""
+                />
+                <div className="leading-relaxed">
+                  <strong className="font-bold font-2xl block">{data.lesson.teacher.name}</strong>
+                  <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
+                </div>
               </div>
-            </div>
+            )}
           </div>
           <div className="flex flex-col gap-4">
             <a href="#" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors ">
